2bd77ccd3e845b49ba45fea7ccb35712218b44ee
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-05-22 18:08:49 +0530

    added constants class for errors

---
+++ includes/classes/Constants.php
@@ -0,0 +1,12 @@
+<?php
+    class Constants{
+        public static $passwordsDoNotMatch="Passwords do not match";
+        public static $passwordNotAlphaNumeric="Password should only contain numbers and letters";
+        public static $passwordCharacters="Your password must be between 6 and 30 characters";
+        public static $emailInvalid="Email is invalid";
+        public static $emailsDoNotMatch="Your emails does not match";
+        public static $lastNameCharacters="Your last name must be between 2 and 25 characters";
+        public static $firstNameCharacters="Your first name must be between 2 and 25 characters ";
+        public static $userNameCharacters="Your username must be between 5 and 25 characters ";
+    }
+?>
